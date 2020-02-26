ddabfeaf68f56888ebb0be065e5b29e563bff43a
Wilmai <artegraff.stile@gmail.com>
2020-02-25 21:52:35 -0300

    Escribiendo Html y componente formulario.js
    Escribiendo Html y componente formulario.js enlazando componentes

---
+++ src/components/Formulario.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Formulario = () => {
+    return (
+        <form>
+            <div className="row">
+                <div className="form-group col-md-8">
+                    <input
+                        type="text"
+                        className="form-control form-control-lg"
+                        placeholder="Busca una imagen ejemplo: futbol o café"
+
+                    />
+                </div>
+
+                <div className="form-group col-md-4">
+                    <input
+                        type="submit"
+                        className="btn btn-lg btn-danger btn-block"
+                        placeholder="Busca una imagen ejemplo: futbol o café"
+
+                    />
+                </div>
+            </div>
+        </form>
+     );
+}
+
+export default Formulario;
